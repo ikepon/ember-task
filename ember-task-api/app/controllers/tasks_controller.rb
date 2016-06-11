ad42b9118c27341a88505b42f99048760734f3da
ikepon <tatsuyanoheya@gmail.com>
2016-06-11 23:08:18 +0900

    Rails側で渡ってきたJSONをdeserialize #1

--- ember-task-api/app/controllers/tasks_controller.rb
+++ ember-task-api/app/controllers/tasks_controller.rb
@@ -46,6 +46,6 @@ class TasksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def task_params
-      params.require(:task).permit(:title, :description, :complete, :deadline)
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
