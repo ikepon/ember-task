493b831b8464c3449ae78a9bb452e1b394bd7b01
ikepon <tatsuyanoheya@gmail.com>
2016-06-12 14:47:00 +0900

    tasks/edit のベース作成 #1

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('tasks', { path: '/' }, function() {
-    this.route('edit');
+    this.route('edit', { path: '/:task_id/edit' });
   });
 });
 
