3d887d6c01bb4603294e8dfdb5ebe4f9f639ec29
ikepon <tatsuyanoheya@gmail.com>
2016-06-12 15:40:17 +0900

    タスク作成 action 作成 #1

--- app/routes/tasks/new.js
+++ app/routes/tasks/new.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('task');
+  },
+
+  actions: {
+    saveTask(model) {
+      model.save().then(() => this.transitionTo('tasks'));
+    },
+
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
