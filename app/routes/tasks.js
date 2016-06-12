8e35adf0eee6c5e4a2ed6a74b4e9949f366dae94
ikepon <tatsuyanoheya@gmail.com>
2016-06-12 15:40:38 +0900

    削除ボタン追加 #1

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('task');
+  },
+
+  actions: {
+    deleteTask(task) {
+      task.destroyRecord();
+    }
   }
 });
