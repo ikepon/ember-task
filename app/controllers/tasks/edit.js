30030ba20267751a9b02772f246c9c05c094115d
ikepon <tatsuyanoheya@gmail.com>
2016-06-12 15:01:56 +0900

    編集したタスクを保存 #1

--- app/controllers/tasks/edit.js
+++ app/controllers/tasks/edit.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled: Ember.computed.empty('model.title')
+  isDisabled: Ember.computed.empty('model.title'),
+
+  actions: {
+    saveTask(model) {
+      model.save();
+    }
+  }
 });
